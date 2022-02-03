858ee57f2d2d64003f742f9686a2475fcff10bee
lunseil <lunseil@qq.com>
2022-02-03 21:22:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/10.10.10.222/g' openwrt/package/base-files/files/bin/confi
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Routes'' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. 设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #4. 版本号里显示一个自己的名字
 #sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
