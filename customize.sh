18a36a7dc1f8848604f60e56582f0a64dc35a8ac
lunseil <lunseil@qq.com>
2022-02-03 20:58:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@
 # Lisence: MIT
 # Author: Lunseil
 #=================================================
-#1. 修改默认的IP
+#1. 修改默认的IP以
 sed -i 's/192.168.1.1/10.10.10.222/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 修改主机名
@@ -15,10 +15,11 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/f
 
 #4. 版本号里显示一个自己的名字
 #sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#5. 修改插件名字
-#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' openwrt/package/lean/luci-app-sfe/po/zh-cn/sfe.po
-        
-#6. 修改上游DNS
+   
+#5. 修改上游DNS
 #sed -i "2i # network config" openwrt/package/lean/default-settings/files/zzz-default-settings
-#sed -i "3i uci set network.lan.dns='114.114.114.114'"  openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i "3i uci set network.lan.dns='114.114.114.114'" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#6. 修改WAN口为PPPOE
+#sed -i 's/username/5161851ST@MYADSL/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/password/240629/g' openwrt/package/base-files/files/bin/config_generate
